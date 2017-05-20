ae21774ceaf7a651ae3f765525da7f0aa51e67b2
Jackson Egan <jackson@sneagan.com>
2017-05-20 00:31:23 -0700

    Logins one day

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
     let mut router = Router::new();
     router.get("/", index_handler, "index");
     router.get("/:query", query_handler, "queryEndpoint");
+    router.get("login/:system", login_handler, "loginEndpoint");
 
     Iron::new(router).http("localhost:3000").unwrap();
 }
@@ -27,6 +28,38 @@ fn index_handler(req: &mut Request) -> IronResult<Response> {
 
 fn query_handler(req: &mut Request) -> IronResult<Response> {
     let string_from_req_param = String::from(req.extensions.get::<Router>().unwrap().find("query").unwrap());
+    let test_var = Responder { message: string_from_req_param};
+    let payload = json::encode(&test_var).unwrap();
+    let my_response = Response::with((status::Ok, payload));
+
+    Ok(my_response)
+}
+
+fn login_handler(req: &mut Request) -> IronResult<Response> {
+    let play_system = String::from(req.extensions.get::<Router>().unwrap().find("system").unwrap());
+    let user = String::from(req.extensions.get::<Router>().unwrap().find("user").unwrap());
+    let pass = String::from(req.extensions.get::<Router>().unwrap().find("password").unwrap());
+
+
+    match play_system {
+        "xbox" => xbox_login(user, pass),
+        "ps"   => ps_login(user, pass),
+        "bnet" => bnet_login(user, pass),
+        _      => fail(user, pass)
+    }
+
+    fn xbox_login(cred_name: String, cred_pass: String) {
+
+    }
+
+    fn ps_login(cred_name: String, cred_pass: String) {
+
+    }
+
+    fn pc_login(cred_name: String, cred_pass: String) {
+
+    }
+
     let test_var = Responder { message: string_from_req_param};
     let payload = json::encode(&test_var).unwrap();
     let my_response = Response::with((status::Ok, payload));
